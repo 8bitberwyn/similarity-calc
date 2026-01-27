a7f18d8808f08709d2fa6b7bfbaf38602531a05c
8bitberwyn <berwynzhu@gmail.com>
2026-01-28 01:13:48 +1100

    Added donate button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import ProtectedRoute from './components/ProtectedRoute'
+import DonateButton from './components/DonateButton' // Check this import path
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
@@ -13,18 +14,28 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          
-          {/* Protected routes - require authentication */}
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path="/setup" element={<ProtectedRoute><Setup /></ProtectedRoute>} />
-          <Route path="/compare" element={<ProtectedRoute><Compare /></ProtectedRoute>} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </AuthProvider>
+  )
+}
+
+// Separate component to access useAuth
+function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route path="/setup" element={<ProtectedRoute><Setup /></ProtectedRoute>} />
+        <Route path="/compare" element={<ProtectedRoute><Compare /></ProtectedRoute>} />
+      </Routes>
+      
+      {/* Donate button - always show when logged in */}
+      <DonateButton />
+    </>
   )
 }
 
